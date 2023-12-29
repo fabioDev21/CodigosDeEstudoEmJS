e6c2cdb8c420416bd199c8ccfb04c6d9f513a929
peije21 <fabiotwist1@gmail.com>
2023-12-28 21:14:39 -0300

    exercicio de aula

---
+++ Aulas/6.Node/exercicio-node.js
@@ -0,0 +1,21 @@
+// esse exercício foi feito como solução para o desafio proposto na aula 155
+
+const url = "http://files.cod3r.com.br/curso-js/funcionarios.json"
+const axios = require("axios")
+
+axios.get(url).then(response => {
+    const funcionarios = response.data
+    const paisF = []
+    funcionarios.map(f => {
+        if(f.pais === 'China'){
+            if(f.genero === 'F'){
+                if(f.salario){
+                    paisF.push(f.salario)
+                }
+            }
+        }
+    })
+    console.log(paisF.sort((a,b) => a-b)[0])
+})
+
+
